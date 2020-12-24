f4eef7d14e76879f99dd015cee0d4312d0dc928b
AzharSy3d <azharuworld@gmail.com>
2020-12-25 00:42:53 +0530

    LCPRJ-0001 : Create Spring Boot Project Structure, Closes #1

---
+++ src/test/java/com/azharuworld/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azharuworld.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
