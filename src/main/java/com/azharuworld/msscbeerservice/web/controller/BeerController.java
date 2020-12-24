c02a983efabc759eb17842e7003a65df675bba20
AzharSy3d <azharuworld@gmail.com>
2020-12-25 01:05:23 +0530

    LCPRJ-0003 : Create Beer Service Controller, closes #3

---
+++ src/main/java/com/azharuworld/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,31 @@
+package com.azharuworld.msscbeerservice.web.controller;
+
+import com.azharuworld.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeer(@PathVariable UUID beerId){
+        //todo Impl
+        return  new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
+        //todo Impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@PathVariable UUID id, @RequestBody BeerDto beerDto){
+        //todo Impl
+        return  new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
