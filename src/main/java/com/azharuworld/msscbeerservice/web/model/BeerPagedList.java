93e19f85e40c7264a1ca3ff615fb56af7766c677
AzharSy3d <azharuworld@gmail.com>
2021-01-03 00:17:16 +0530

    LCPRJ-0013 : Adding jsoncreator to BeerPage,closes #14

--- src/main/java/com/azharuworld/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/com/azharuworld/msscbeerservice/web/model/BeerPagedList.java
@@ -1,11 +1,32 @@
 package com.azharuworld.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public class BeerPagedList extends PageImpl<BeerDto> {
+
+
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
+                         @JsonProperty("number") int number,
+                         @JsonProperty("size") int size,
+                         @JsonProperty("totalElements") Long totalELements,
+                         @JsonProperty("pageable") JsonNode pageable,
+                         @JsonProperty("totalPages") int totalPages,
+                         @JsonProperty("sort") JsonNode sort,
+                         @JsonProperty("first") boolean first,
+                         @JsonProperty("last") boolean last,
+                         @JsonProperty("numberOfElements") int numberOfElements) {
+        super(content, PageRequest.of(number,size),totalELements);
+    }
+
     public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
     }
